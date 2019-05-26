76e08a6824b9f6663005617174b210c6d9a6e0ca
Han Xu <hanxu27@gmail.com>
2019-05-25 22:01:50 -0400

    migrations and associations

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  resources :teams
+  resources :deciders
+  resources :coaches
+  resources :evaluations
+  resources :tryouts
+  resources :players
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
