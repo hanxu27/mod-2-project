088d7c51593126c4b3e9618d21f6307b991996d7
Han Xu <hanxu27@gmail.com>
2019-05-27 00:10:55 -0400

    tryout crud

--- app/models/tryout.rb
+++ app/models/tryout.rb
@@ -5,4 +5,9 @@ class Tryout < ApplicationRecord
   
   validates :season, inclusion: {in: 2019..2100, message: "must be the year"}
   validates :age_group, inclusion: 10..18
+  validates :reach, numericality: true
+  validates :approach_jump, numericality: true
+  validates :block_jump, numericality: true
+  validates :point_touch, numericality: true
+  validates :dash, numericality: true
 end
